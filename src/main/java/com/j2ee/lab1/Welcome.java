e9e8bf57f373d9422063e8ce4d17280bc2be6e5b
ManpreetKGulati <144505454+ManpreetKGulati@users.noreply.github.com>
2024-09-06 18:10:16 -0400

    update

--- src/main/java/com/j2ee/lab1/Welcome.java
+++ src/main/java/com/j2ee/lab1/Welcome.java
@@ -13,6 +13,6 @@ public class Welcome extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Welcome to J2EE Web Components Lab</h1>");
+        response.getWriter().println("<h1>Hello! This is J2EE lab 1</h1>");
     }
 }
