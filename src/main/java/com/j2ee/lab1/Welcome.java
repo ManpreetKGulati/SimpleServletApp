d1d0501ba2d8e5c88f5236d66fbe2033b3964dea
“MANPREET <gulatimk.ca@gmail.com>
2024-09-05 21:06:55 -0400

    Lab1

---
+++ src/main/java/com/j2ee/lab1/Welcome.java
@@ -0,0 +1,18 @@
+package com.j2ee.lab1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Welcome extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Welcome to J2EE Web Components Lab</h1>");
+    }
+}
